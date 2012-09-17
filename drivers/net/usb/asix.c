bd128ad246582ec8e44531396428b680d5a87382
Søren Holm <sgh@sgh.dk>
2012-09-17 21:50:57 +0000

    asix: Support DLink DUB-E100 H/W Ver C1
    commit ed3770a9cd5764a575b83810ea679bbff2b03082 upstream.
    
    Change-Id: I15d28a891f0f5e6ae26f6232b218e0068872077f
    Signed-off-by: Søren Holm <sgh@sgh.dk>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/usb/asix.c
+++ drivers/net/usb/asix.c
@@ -3613,6 +3613,10 @@ static const struct usb_device_id	products [] = {
 	USB_DEVICE (0x2001, 0x3c05),
 	.driver_info =  (unsigned long) &dlink_dub_e100b_info,
 }, {
+	// DLink DUB-E100 H/W Ver C1
+	USB_DEVICE (0x2001, 0x1a02),
+	.driver_info = (unsigned long) &ax88772_info,
+}, {
 	// DLink DUB-E100B
 	USB_DEVICE (0x07d1, 0x3c05),
 	.driver_info =  (unsigned long) &dlink_dub_e100b_info,
