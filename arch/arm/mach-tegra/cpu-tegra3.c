533d8e3445a57eb0d61b805ba7ab5a9d3ef7a88e
timduru <timduru@gmail.com>
2014-09-15 17:19:23 +0200

    Increase balanced speed target to 70% max

--- arch/arm/mach-tegra/cpu-tegra3.c
+++ arch/arm/mach-tegra/cpu-tegra3.c
@@ -70,7 +70,7 @@ module_param(idle_bottom_freq, uint, 0644);
 static int mp_overhead = 10;
 module_param(mp_overhead, int, 0644);
 
-static int balance_level = 60;
+static int balance_level = 70;
 module_param(balance_level, int, 0644);
 
 static struct clk *cpu_clk;
